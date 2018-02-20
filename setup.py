a541988328386df0a47c1c1bf25bae0cc9801547
Andrej Babic <andrej.babic@psi.ch>
2018-02-20 15:19:30 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="sf_dia",
+      version="1.0.0",
+      maintainer="Paul Scherrer Institute",
+      maintainer_email="daq@psi.ch",
+      author="Paul Scherrer Institute",
+      author_email="daq@psi.ch",
+      description="SwissFEL Jungfrau detector integration",
+
+      license="GPL3",
+
+      packages=['sf_dia']
+      )
