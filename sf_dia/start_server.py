de5ed61832aa912e1fec9f9f9039688c618b2ea7
Andrej Babic <andrej.babic@psi.ch>
2018-02-23 10:31:23 +0100

    Improve the startup script

--- sf_dia/start_server.py
+++ sf_dia/start_server.py
@@ -4,6 +4,7 @@ import logging
 import bottle
 from detector_integration_api import config
 from detector_integration_api.client.backend_rest_client import BackendClient
+from detector_integration_api.client.cpp_writer_client import CppWriterClient
 from detector_integration_api.client.detector_cli_client import DetectorClient
 from detector_integration_api.rest_api.rest_server import register_rest_interface
 from mflow_nodes import NodeClient
@@ -13,13 +14,16 @@ from sf_dia import manager
 _logger = logging.getLogger(__name__)
 
 
-def start_integration_server(host, port, backend_url, writer_url, writer_instance_name,
+def start_integration_server(host, port,
+                             backend_api_url, backend_stream_url, writer_port,
                              bsread_url, bsread_instance_name, disable_bsread):
-    _logger.info("Starting integration REST API with:\nBackend url: %s\nWriter url: %s\nWriter instance name: %s\n",
-                 backend_url, writer_url, writer_instance_name)
 
-    backend_client = BackendClient(backend_url)
-    writer_client = NodeClient(writer_url, writer_instance_name)
+    _logger.info("Starting integration REST API with:\nBackend url: %s\nBackend stream: "
+                 "%s\nWriter port: %s\nbsread url: %s\n",
+                 backend_api_url, backend_stream_url, str(writer_port), bsread_url)
+
+    backend_client = BackendClient(backend_api_url)
+    writer_client = CppWriterClient(backend_stream_url, writer_port)
     bsread_client = NodeClient(bsread_url, bsread_instance_name)
     detector_client = DetectorClient()
 
@@ -49,14 +53,17 @@ def main():
     parser.add_argument("--log_level", default=config.DEFAULT_LOGGING_LEVEL,
                         choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
                         help="Log level to use.")
-    parser.add_argument("-b", "--backend_url", default=config.DEFAULT_BACKEND_URL,
+
+    parser.add_argument("-b", "--backend_url", default="http://localhost:8080",
                         help="Backend REST API url.")
-    parser.add_argument("-w", "--writer_url", default=config.DEFAULT_WRITER_URL,
-                        help="Writer REST API url.")
+    parser.add_argument("-s", "--backend_stream", default="tcp://localhost:40000",
+                        help="Output stream address from the backend.")
+
+    parser.add_argument("-w", "--writer_port", type=int, default=10001,
+                        help="Writer REST API port.")
+
     parser.add_argument("-s", "--bsread_url", default=config.DEFAULT_BSREAD_URL,
                         help="Writer REST API url.")
-    parser.add_argument("--writer_instance_name", default=config.DEFAULT_WRITER_INSTANCE_NAME,
-                        help="Writer instance name.")
     parser.add_argument("--bsread_instance_name", default=config.DEFAULT_BSREAD_INSTANCE_NAME,
                         help="Writer instance name.")
     parser.add_argument("--disable_bsread", action='store_true',
@@ -68,10 +75,8 @@ def main():
     logging.basicConfig(level=arguments.log_level, format='[%(levelname)s] %(message)s')
 
     start_integration_server(arguments.interface, arguments.port,
-                             arguments.backend_url,
-                             arguments.writer_url, arguments.writer_instance_name,
-                             arguments.bsread_url, arguments.bsread_instance_name,
-                             arguments.disable_bsread)
+                             arguments.backend_url, arguments.backend_stream, arguments.writer_port,
+                             arguments.bsread_url, arguments.bsread_instance_name, arguments.disable_bsread)
 
 
 if __name__ == "__main__":
