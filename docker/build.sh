ee360050038cfe358ed45d67f573d00ca5eecaa2
Andrej Babic <andrej.babic@psi.ch>
2018-02-23 08:37:02 +0100

    Fix docker build

--- docker/build.sh
+++ docker/build.sh
@@ -2,9 +2,9 @@
 VERSION=0.1.9
 
 # Build the docker image.
-docker build --no-cache=true -t docker.psi.ch:5000/detector_api_server .
+docker build --no-cache=true -t docker.psi.ch:5000/sf_dia .
 
 # Push it to our repo.
-docker tag docker.psi.ch:5000/detector_api_server docker.psi.ch:5000/detector_api_server:$VERSION
-docker push docker.psi.ch:5000/detector_api_server:$VERSION
-docker push docker.psi.ch:5000/detector_api_server
+docker tag docker.psi.ch:5000/sf_dia docker.psi.ch:5000/sf_dia:$VERSION
+docker push docker.psi.ch:5000/sf_dia:$VERSION
+docker push docker.psi.ch:5000/sf_dia
