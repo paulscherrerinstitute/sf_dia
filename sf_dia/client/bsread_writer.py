a4f244c54b85818e6585fc92b265bae19ef46b55
Andrej Babic <andrej.babic@psi.ch>
2018-02-28 16:27:19 +0100

    Create bsread writer client

---
+++ sf_dia/client/bsread_writer.py
@@ -0,0 +1,162 @@
+import os.path
+import requests
+import json
+
+from subprocess import Popen
+from datetime import datetime
+from logging import getLogger
+from time import sleep
+
+from detector_integration_api import config
+
+_logger = getLogger(__name__)
+
+
+class BsreadWriterClient(object):
+    PROCESS_STARTUP_PARAMETERS = ("output_file", "user_id")
+
+    def __init__(self, stream_url, writer_executable, writer_port, log_folder=None):
+
+        self.stream_url = stream_url
+        self.writer_executable = writer_executable
+        self.writer_port = writer_port
+
+        self.log_folder = None
+        if log_folder is not None:
+
+            if not os.path.exists(log_folder):
+                raise ValueError("Provided writer log folder '%s' does not exist." % log_folder)
+
+            self.log_folder = log_folder
+
+        self.writer_url = config.WRITER_PROCESS_URL_FORMAT % writer_port
+        self.writer_parameters = None
+
+        self.process = None
+        self.process_log_file = None
+
+    @staticmethod
+    def _sanitize_parameters(parameters):
+
+        return {key: parameters[key] for key in parameters if key not in BsreadWriterClient.PROCESS_STARTUP_PARAMETERS}
+
+    def start(self):
+
+        if self.is_running():
+            raise RuntimeError("Writer process already running. Cannot start new one until old one is still alive.")
+
+        if not self.writer_parameters:
+            raise ValueError("Writer parameters not set.")
+
+        timestamp = datetime.now().strftime(config.WRITER_PROCESS_LOG_FILENAME_TIME_FORMAT)
+
+        # If the log folder is not specified, redirect the logs to /dev/null.
+        if self.log_folder is not None:
+            log_filename = os.path.join(self.log_folder, "bsread_writer-%s.log" % timestamp)
+        else:
+            log_filename = os.devnull
+
+        _logger.debug("Creating log file '%s'.", log_filename)
+        self.process_log_file = open(log_filename, 'w')
+        self.process_log_file.write("Parameters:\n%s\n" % json.dumps(self.writer_parameters, indent=4))
+        self.process_log_file.flush()
+
+        writer_command_format = "sh " + self.writer_executable + " %s %s %s"
+        writer_command = writer_command_format % (self.stream_url,
+                                                  self.writer_parameters["output_file"],
+                                                  self.writer_parameters.get("user_id", -1))
+
+        _logger.debug("Starting writer with command '%s'.", writer_command)
+        self.process = Popen(writer_command, shell=True, stdout=self.process_log_file, stderr=self.process_log_file)
+
+        sleep(config.WRITER_PROCESS_STARTUP_WAIT_TIME)
+
+        process_parameters = self._sanitize_parameters(self.writer_parameters)
+        _logger.debug("Setting process parameters: %s", process_parameters)
+
+        for _ in range(config.WRITER_PROCESS_RETRY_N):
+
+            try:
+                response = requests.post(self.writer_url + "/parameters", json=process_parameters,
+                                         timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+
+                if response.status_code != 200:
+                    continue
+
+                break
+
+            except:
+                sleep(config.WRITER_PROCESS_RETRY_DELAY)
+        else:
+            _logger.warning("Terminating writer process because it did not respond in the specified time.")
+
+            requests.get(self.writer_url + "/kill", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+
+            self.process.wait(timeout=config.WRITER_PROCESS_TERMINATE_TIMEOUT)
+
+            self.process.terminate()
+
+            raise RuntimeError("Count not start writer process in time. Check writer logs.")
+
+    def stop(self):
+
+        _logger.debug("Stopping writer.")
+
+        if self.is_running():
+            requests.get(self.writer_url + "/stop", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+
+            try:
+                self.process.wait(timeout=config.WRITER_PROCESS_TERMINATE_TIMEOUT)
+
+            except:
+                _logger.warning("Terminating writer process because it did not stop in the specified time.")
+
+                requests.get(self.writer_url + "/kill", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT)
+
+                self.process.wait(timeout=config.WRITER_PROCESS_TERMINATE_TIMEOUT)
+
+                self.process.terminate()
+
+                raise RuntimeError("Writer process was terminated because it did not stop in time. "
+                                   "Acquisition file maybe corrupted.")
+
+        else:
+            _logger.debug("Writer process is not running.")
+
+        if self.process_log_file:
+            self.process_log_file.flush()
+            self.process_log_file.close()
+
+        self.process = None
+        self.process_log_file = None
+
+    def is_running(self):
+        return self.process is not None and self.process.poll() is None
+
+    def get_status(self):
+
+        # Writer is running. Get status from the process.
+        if self.is_running():
+            status = requests.get(self.writer_url + "/status",
+                                  timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT).json()
+
+            return status["status"]
+
+        return "stopped"
+
+    def set_parameters(self, writer_parameters):
+        self.writer_parameters = writer_parameters
+
+    def reset(self):
+        _logger.debug("Resetting writer.")
+
+        self.stop()
+
+        self.writer_parameters = None
+
+    def get_statistics(self):
+
+        if not self.is_running():
+            return {}
+
+        return requests.get(self.writer_url + "/statistics", timeout=config.WRITER_PROCESS_COMMUNICATION_TIMEOUT).json()
--- sf_dia/start_server.py
+++ sf_dia/start_server.py
@@ -6,9 +6,9 @@ from detector_integration_api import config
 from detector_integration_api.client.backend_rest_client import BackendClient
 from detector_integration_api.client.cpp_writer_client import CppWriterClient
 from detector_integration_api.rest_api.rest_server import register_rest_interface
-from mflow_nodes import NodeClient
 
 from sf_dia import manager
+from sf_dia.client.bsread_writer import BsreadWriterClient
 from sf_dia.client.detector_timing_cli_client import DetectorTimingClient
 
 _logger = logging.getLogger(__name__)
@@ -16,13 +16,12 @@ _logger = logging.getLogger(__name__)
 
 def start_integration_server(host, port,
                              backend_api_url, backend_stream_url, writer_port,
-                             bsread_url, bsread_instance_name, disable_bsread,
+                             bsread_stream, bsread_port, bsread_executable, disable_bsread,
                              timing_pv, timing_start_code, timing_stop_code,
                              writer_executable, writer_log_folder):
-
     _logger.info("Starting integration REST API with:\nBackend url: %s\nBackend stream: "
-                 "%s\nWriter port: %s\nbsread url: %s\n",
-                 backend_api_url, backend_stream_url, str(writer_port), bsread_url)
+                 "%s\nWriter port: %s\nbsread_stream: %s\n",
+                 backend_api_url, backend_stream_url, str(writer_port), bsread_stream)
 
     _logger.info("Using writer executable '%s' and writing writer logs to '%s'.", writer_executable, writer_log_folder)
 
@@ -31,7 +30,12 @@ def start_integration_server(host, port,
                                     writer_executable=writer_executable,
                                     writer_port=writer_port,
                                     log_folder=writer_log_folder)
-    bsread_client = NodeClient(bsread_url, bsread_instance_name)
+
+    bsread_client = BsreadWriterClient(stream_url=bsread_stream,
+                                       writer_executable=bsread_executable,
+                                       writer_port=bsread_port,
+                                       log_folder=writer_log_folder)
+
     detector_client = DetectorTimingClient(timing_pv, timing_start_code, timing_stop_code)
 
     integration_manager = manager.IntegrationManager(writer_client=writer_client,
@@ -73,10 +77,12 @@ def main():
     parser.add_argument("--writer_log_folder", type=str, default="/var/log/h5_zmq_writer",
                         help="Log directory for writer logs.")
 
-    parser.add_argument("-bsread_url", "--bsread_url", default=config.DEFAULT_BSREAD_URL,
-                        help="Writer REST API url.")
-    parser.add_argument("--bsread_instance_name", default=config.DEFAULT_BSREAD_INSTANCE_NAME,
-                        help="Writer instance name.")
+    parser.add_argument("--bsread_stream", default="tcp://127.0.0.1:8082",
+                        help="Address of the bsread buffer stream.")
+    parser.add_argument("--bsread_executable", default="/home/writer/start_bsread_writer.sh",
+                        help="Executable to start the bsread writer.")
+    parser.add_argument("--bsread_port", default=10002,
+                        help="Bsread REST API port.")
     parser.add_argument("--disable_bsread", action='store_true',
                         help="Disable the bsread writer at startup.")
 
@@ -92,10 +98,18 @@ def main():
     # Setup the logging level.
     logging.basicConfig(level=arguments.log_level, format='[%(levelname)s] %(message)s')
 
-    start_integration_server(arguments.interface, arguments.port,
-                             arguments.backend_url, arguments.backend_stream, arguments.writer_port,
-                             arguments.bsread_url, arguments.bsread_instance_name, arguments.disable_bsread,
-                             arguments.timing_pv, arguments.timing_start_code, arguments.timing_stop_code,
+    start_integration_server(host=arguments.interface,
+                             port=arguments.port,
+                             backend_api_url=arguments.backend_url,
+                             backend_stream_url=arguments.backend_stream,
+                             writer_port=arguments.writer_port,
+                             bsread_stream=arguments.bsread_stream,
+                             bsread_port=arguments.bsread_port,
+                             bsread_executable=arguments.bsread_executable,
+                             disable_bsread=arguments.disable_bsread,
+                             timing_pv=arguments.timing_pv,
+                             timing_start_code=arguments.timing_start_code,
+                             timing_stop_code=arguments.timing_stop_code,
                              writer_executable=arguments.writer_executable,
                              writer_log_folder=arguments.writer_log_folder)
 
