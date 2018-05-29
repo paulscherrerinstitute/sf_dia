1cd3b927f1554f0ae6777112e08016515a0e6437
Andrej Babic <andrej.babic@psi.ch>
2018-05-29 13:39:33 +0200

    Change runner script of server

--- sf_dia/start_server.py
+++ sf_dia/start_server.py
@@ -8,7 +8,7 @@ from detector_integration_api.client.cpp_writer_client import CppWriterClient
 from detector_integration_api.rest_api.rest_server import register_rest_interface
 
 from sf_dia import manager
-from sf_dia.client.bsread_writer_client import BsreadWriterClient
+from sf_dia.client.databuffer_writer_client import DataBufferWriterClient
 from sf_dia.client.detector_timing_cli_client import DetectorTimingClient
 
 _logger = logging.getLogger(__name__)
@@ -16,12 +16,12 @@ _logger = logging.getLogger(__name__)
 
 def start_integration_server(host, port,
                              backend_api_url, backend_stream_url, writer_port,
-                             bsread_stream, bsread_port, bsread_executable, disable_bsread,
+                             broker_url, disable_bsread,
                              timing_pv, timing_start_code, timing_stop_code,
                              writer_executable, writer_log_folder):
     _logger.info("Starting integration REST API with:\nBackend url: %s\nBackend stream: "
-                 "%s\nWriter port: %s\nbsread_stream: %s\n",
-                 backend_api_url, backend_stream_url, str(writer_port), bsread_stream)
+                 "%s\nWriter port: %s\nbroker_url: %s\n",
+                 backend_api_url, backend_stream_url, str(writer_port), broker_url)
 
     _logger.info("Using writer executable '%s' and writing writer logs to '%s'.", writer_executable, writer_log_folder)
 
@@ -31,10 +31,7 @@ def start_integration_server(host, port,
                                     writer_port=writer_port,
                                     log_folder=writer_log_folder)
 
-    bsread_client = BsreadWriterClient(stream_url=bsread_stream,
-                                       writer_executable=bsread_executable,
-                                       writer_port=bsread_port,
-                                       log_folder=writer_log_folder)
+    bsread_client = DataBufferWriterClient(broker_url=broker_url)
 
     detector_client = DetectorTimingClient(timing_pv, timing_start_code, timing_stop_code)
 
@@ -77,12 +74,8 @@ def main():
     parser.add_argument("--writer_log_folder", type=str, default="/var/log/h5_zmq_writer",
                         help="Log directory for writer logs.")
 
-    parser.add_argument("--bsread_stream", default="tcp://127.0.0.1:8082",
-                        help="Address of the bsread buffer stream.")
-    parser.add_argument("--bsread_executable", default="/home/writer/start_bsread_writer.sh",
-                        help="Executable to start the bsread writer.")
-    parser.add_argument("--bsread_port", default=10002,
-                        help="Bsread REST API port.")
+    parser.add_argument("--broker_url", default="http://localhost:10002",
+                        help="Address of the bsread broker REST api.")
     parser.add_argument("--disable_bsread", action='store_true',
                         help="Disable the bsread writer at startup.")
 
@@ -103,9 +96,7 @@ def main():
                              backend_api_url=arguments.backend_url,
                              backend_stream_url=arguments.backend_stream,
                              writer_port=arguments.writer_port,
-                             bsread_stream=arguments.bsread_stream,
-                             bsread_port=arguments.bsread_port,
-                             bsread_executable=arguments.bsread_executable,
+                             broker_url=arguments.broker_url,
                              disable_bsread=arguments.disable_bsread,
                              timing_pv=arguments.timing_pv,
                              timing_start_code=arguments.timing_start_code,
