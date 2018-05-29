cd91d2ed74b9bd5aef8d1204e323e0032150ef64
Andrej Babic <andrej.babic@psi.ch>
2018-05-29 13:39:54 +0200

    New library version
    - Add broker client

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_dia",
-      version="1.3.0",
+      version="1.4.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
