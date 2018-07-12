4538862e5af81e7a54137361c6265d23e1bfdbf8
Andrej Babic <andrej.babic@psi.ch>
2018-07-12 17:13:05 +0200

    Added n_modules as a detector config parameter

--- sf_dia/sf_config_bernina.py
+++ sf_dia/sf_config_bernina.py
@@ -1,7 +1,7 @@
 # Bernina configuration:
 available_detectors = {}
-available_detectors['ESB_JF1pM'] =    {'detector_id': 1, 'backend_api_url': 'http://localhost:8080', 'backend_stream_url': 'tcp://localhost:40000', 'writer_port': 10010}
-available_detectors['ESB_I0'] =       {'detector_id': 2, 'backend_api_url': 'http://localhost:8081', 'backend_stream_url': 'tcp://localhost:40001', 'writer_port': 10011}
-available_detectors['ESB_fluo'] =     {'detector_id': 3, 'backend_api_url': 'http://localhost:8082', 'backend_stream_url': 'tcp://localhost:40002', 'writer_port': 10012}
-available_detectors['ESB_stripsel'] = {'detector_id': 4, 'backend_api_url': 'http://localhost:8083', 'backend_stream_url': 'tcp://localhost:40003', 'writer_port': 10013}
+available_detectors['ESB_JF1pM'] =    {'detector_id': 1, 'backend_api_url': 'http://localhost:8080', 'backend_stream_url': 'tcp://localhost:40000', 'writer_port': 10010, 'n_modules': 1}
+available_detectors['ESB_I0'] =       {'detector_id': 2, 'backend_api_url': 'http://localhost:8081', 'backend_stream_url': 'tcp://localhost:40001', 'writer_port': 10011, 'n_modules': 1}
+available_detectors['ESB_fluo'] =     {'detector_id': 3, 'backend_api_url': 'http://localhost:8082', 'backend_stream_url': 'tcp://localhost:40002', 'writer_port': 10012, 'n_modules': 1}
+available_detectors['ESB_stripsel'] = {'detector_id': 4, 'backend_api_url': 'http://localhost:8083', 'backend_stream_url': 'tcp://localhost:40003', 'writer_port': 10013, 'n_modules': 1}
 
